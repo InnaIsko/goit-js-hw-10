e0c11d53060edbd87025f20f5fa9de08c2ddc12e
Inna <isko.inna.94@gmail.com>
2022-10-23 17:48:54 +0300

    2

---
+++ src/fetchCountries.js
@@ -0,0 +1,7 @@
+function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v2/name/${name}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => console.log(data));
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './css/styles.css';
+// import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+const searchBox = document.querySelector('#search-box');
+
+searchBox.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+function onInput(event) {
+  const inputValue = event.currentTarget.value;
+  console.log(inputValue);
+  fetchCountries(inputValue);
+}
+function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(data => console.log(data));
+}
